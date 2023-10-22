b38cf21eb2a4375a3803e012b937dd2d0814ddce
GreedMora <148688795+GreedMora@users.noreply.github.com>
2023-10-22 15:28:37 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const themeSwitch = document.getElementById("themeSwitch");
+const root = document.querySelector(":root");
+
+themeSwitch.addEventListener("change", () => {
+  if (themeSwitch.checked) {
+    root.style.setProperty("--text-color: #1a1a1a;");
+    root.style.setProperty("--background-color: #090d1f;");
+    root.style.setProperty("--switcher-background: #fff;");
+    root.style.setProperty("--switcher-indicator: #090d1f;");
+  } else {
+    root.style.setProperty("--text-color: #1a1a1a;");
+    root.style.setProperty("--background-color: #fff;");
+    root.style.setProperty("--switcher-background: #090d1f;");
+    root.style.setProperty("--switcher-indicator: #fff;");
+  }
+});
